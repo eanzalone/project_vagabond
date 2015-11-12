f94a333aca27b8e4d47f94ca04d0818abad4bb51
Ling Lu <jojoucsd@gmail.com>
2015-11-12 15:35:36 -0800

    added sign in working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+	def index
+		@user = User.all
+		render :index
+	end
+
+	def new 
+		@user = User.new
+		render :new
+	end
+
+	def create
+		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+		@user = User.create(user_params)
+		login(@user)
+		redirect_to user_path(@user)
+	end
+
+	def show
+		@user = User.find(params[:id])
+		render :show
+	end
+
+end
