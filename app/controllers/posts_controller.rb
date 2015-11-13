91097f1b18454a1a933bca7ec3277fb74abb7907
eanzalone <emily.anzalone@gmail.com>
2015-11-12 18:37:55 -0800

    Fixing Navbar

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+	def index
+		@post = Post.new
+	end
+	def new 
+		@post = Post.new
+		render :new
+	end
+	def create
+		post_params = params.require(:post).permit(:title, :body)
+		p "This is the post: #{params[:post]}"
+		p params
+		Post.create(post_params)
+		redirect_to '/users/2'
+	end
+	def show
+	end
+end
