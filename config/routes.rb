a9a5e99026c4e5970b05d9a9a4d383f785112d66
eanzalone <emily.anzalone@gmail.com>
2015-11-12 16:24:32 -0800

    New Post Page exists

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
   get "/sign_in", to: "sessions#new", as: 'new_session'
-  post "/sessions", to: "sessions#create", as: 'sessions'  
+  post "/sessions", to: "sessions#create", as: 'sessions' 
+
+  get "/post", to: "posts#create"
 end
