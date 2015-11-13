675d9e4f31643613462b94e64ff19700668cae3c
eanzalone <emily.anzalone@gmail.com>
2015-11-12 18:45:09 -0800

    Things should finally be merged properly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,17 +10,9 @@ class PostsController < ApplicationController
 		post_params = params.require(:post).permit(:title, :body)
 		p "This is the post: #{params[:post]}"
 		p params
-<<<<<<< HEAD
 		Post.create(post_params)
-		redirect_to '/users/2'
+		redirect_to '/users/1' #Needs fix redirect
 	end
 	def show
-=======
-		@@posty = Post.create(post_params)
-		redirect_to '/users/2'
-	end
-	def show
-		@@posty
->>>>>>> febdf2e78303b6cfdd56d58da6a19eb2c4b8b8c1
 	end
 end
