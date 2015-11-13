3c1110930db053ba4fb334f8a8302b623f30e99e
jvbarisone@gmail.com <YOUR-EMAIL-ADDRESS>
2015-11-12 21:12:31 -0800

    Add neighborhood column to user model. Added to dropdown to user registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :neighborhood)
 		@user = User.create(user_params)
 		login(@user)
 		redirect_to user_path(@user)
