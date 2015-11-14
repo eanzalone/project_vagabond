5e9e4fbf274cdfb8eecf2e289d869ba2575128be
eanzalone <emily.anzalone@gmail.com>
2015-11-13 17:28:09 -0800

    Fixed Navbar Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,11 @@ Rails.application.routes.draw do
   # get "/users", to: "users#index", as: "users"
 
   get "/sign_up", to: "users#new", as: "sign_up"
-
   # post "/users", to: "users#create"
-
   # get "/users/:id", to: "users#show", as: "user"
-
   get "/login", to: "sessions#new", as: "login"
   get "/logout", to: "sessions#destroy", as: "logout"
-
   post "/sessions", to: "sessions#create", as: 'sessions' 
-
-  get '/posts', to: 'posts#index', as: 'posts' 
 
   resources :users
   resources :posts
